b425843704f8d4a5f329d56792a3754d27a058d5
JUAN  DIEGO TORRES HERNANDEZ <102912628+TsAkashy@users.noreply.github.com>
2022-07-26 12:00:39 -0500

    Update Payment.java

--- src/model/Payment.java
+++ src/model/Payment.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
+/* 
 
-/**
- *
- * @author Usuario
- */
-public class Payment {
-    
+Name: Juan Diego Torres Hernandez 
+Code:20212200521
+Name: Hector Stiven Trujillo Trujillo
+Code:20212200152
+
+*/
+public class Payment extends Client{
+   private int hours_worked;
+   private double payment_hours;
+   private int hours_ext;
+   private int total_hours;
+   private double salary;
+   public Payment (int hours_worked, int payment_hours){
+       this.hours_worked=hours_worked;
+       this.payment_hours=payment_hours;
+   }
+    public int getHours_worked() {
+        return hours_worked;
+    }
+    public void setHours_worked(int hours_worked) {
+        this.hours_worked = hours_worked;
+    }
+    public double getPayment_hours() {
+        return payment_hours;
+    }
+    public void setPayment_hours(int payment_hours) {
+        this.payment_hours = payment_hours;
+    }
+    public int getHours_ext() {
+        return hours_ext;
+    }
+    public void setHours_ext(int hours_ext) {
+        this.hours_ext = hours_ext;
+    }
+    public void generate_payment() {
+   if (hours_worked > 40){
+           hours_ext = hours_worked - 40;
+           total_hours = hours_ext + 40;
+           salary = total_hours * payment_hours;
+    } else 
+      salary = hours_worked * payment_hours; 
+}
 }
